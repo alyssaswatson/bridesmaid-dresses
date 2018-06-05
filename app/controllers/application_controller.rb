3bf4c6bd8d6d6c6042941a8b25a8ac415762c1f3
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-06-05 00:27:10 +0000

    changing routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authentication_required, except: [:index, :create]
+  #protect_from_forgery with: :exception
+  #before_action :authentication_required, except: [:index, :create]
 
-  def authentication_required
-    if !logged_in?
-      redirect_to root_path
-    end
-  end
+  #def authentication_required
+    #if !logged_in?
+      #redirect_to root_path
+    #end
+  #end
 
-  def logged_in?
-    !!current_user
-  end
+  #def logged_in?
+    #!!current_user
+  #end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  #def current_user
+    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  #end
 
-  helper_method :current_user
-  helper_method :logged_in?
+  #helper_method :current_user
+  #helper_method :logged_in?
 end
