9d63b65abfe0ecad8be9862d6da22302403c7003
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-06-15 14:10:11 -0400

    made link to new bridesmaid and form rednering work

--- app/controllers/bridesmaids_controller.rb
+++ app/controllers/bridesmaids_controller.rb
@@ -1,7 +1,12 @@
 class BridesmaidsController < ApplicationController
 
     def new
-        @bridesmaid = Bridesmaid.new
+        #byebug
+        bride = Bride.find(params[:bride_id])
+        #2nd you build a new one
+    
+        @bridesmaid = bride.bridesmaids.build
+        #byebug
       end
 
 end
