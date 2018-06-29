10849f48d1b772c704301648752974c00b0ac3fc
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-06-29 11:25:18 -0400

    adding type validation to dresses

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -4,5 +4,6 @@ class Dress < ActiveRecord::Base
   validates :link, presence: true
   validates :material, presence: true
   validates :length, presence: true
+  validates_numericality_of :length, :on => :create
   accepts_nested_attributes_for :bridesmaids
 end
