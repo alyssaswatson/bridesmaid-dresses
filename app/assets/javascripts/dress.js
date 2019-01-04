da258f974cff7a414b495d75387788d738265805
Alyssa Lee <alyssawatsonlee@gmail.com>
2019-01-03 19:24:06 -0500

    adding dress constructor

---
+++ app/assets/javascripts/dress.js
@@ -0,0 +1,8 @@
+class Dress {
+    constructor(dressJson) {
+        this.id = dressJson.id;
+        this.link = dressJson.link;
+        this.material = dressJson.material;
+        this.length = dressJson.length;
+    };
+};
