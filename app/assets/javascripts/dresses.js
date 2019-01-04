2534e9e577d2f47fd6c70b6e7a6dedb86fe681f9
Alyssa Lee <alyssawatsonlee@gmail.com>
2019-01-03 23:12:18 -0500

    trying to get json to render correctly

--- app/assets/javascripts/dresses.js
+++ app/assets/javascripts/dresses.js
@@ -1,26 +1,14 @@
-$(document).ready(function () {
-
-	$('button#loadDresses').click(function(event) {
-		var url = $(this).data("url")
-	$.get(url)
-	  .done(function( data ) {
-	  	$('#dresses').html("");
-
-	    $.each(data, function(index, value) {
-	    	var newDress = new Dress(value, url);
-	    	
-	    	var dressHtml = newDress.formatDisplay();
-			  $('#dresses').append(dressHtml)
-			});
-
-	  });
-	});	
-});
-
-
-function Dress(dress, url) {
+function Dress(dress) {
         this.id = dress.id;
         this.link = dress.link;
         this.material = dress.material;
         this.length = dress.length;
+};
+
+Dress.prototype.name = function() {
+    return this.material + " " + this.length
+};
+
+function postDresses() {
+    
 };
