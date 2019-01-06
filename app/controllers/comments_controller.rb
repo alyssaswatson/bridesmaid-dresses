88af145e94351ad1b986a4f032ff55cad8a21c6a
Alyssa Lee <alyssawatsonlee@gmail.com>
2019-01-06 14:32:57 -0500

    changing how javascript renders

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,23 +7,33 @@ class CommentsController < ApplicationController
         @comments = @dress.comments.all
         render json: @comments
     end
+
+    def new
+        @dress = Dress.find(params[:dress_id])
+        @comment = Comment.new
+        @url = new_dress_comment_path(@dress)
+    end
     
     def create 
         @dress = Dress.find(params[:dress_id])
         @comment = @dress.comments.new(comment_params)
-        @comment.save!
-        
-        respond_to do |format|
-            format.js
-        end
-        
-    end
+        if @comment.save
+			redirect_to dress_comment_path(@comment.dress_id, @comment)
+		else 
+			flash[:notice] = "This couldn't be saved"
+			redirect_to new_dress_comment_path(@dress)
+		end
+	end
 
     def show
         #byebug
 		@comment= Comment.find(params[:id])
 	end
-	
+    
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+    end
 
 private
 
