6f743790a4604b8d4fd8bcba8e6f6fd59ef8c13d
Alyssa Lee <alyssawatsonlee@gmail.com>
2019-01-05 22:34:02 -0500

    working on building Comments Controller and changin link on show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
+require 'byebug'
+
 class CommentsController < ApplicationController
+    
+    def index
+        byebug
+		@dress = Dress.find(params[:landlord_id])
+		@buildings = @landlord.buildings.all
+		render json: @buildings
+	end
+
+
 end
