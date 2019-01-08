e01ae2cf7ffae7560493ebeb40f39f1b6fd86db5
Alyssa Lee <alyssawatsonlee@gmail.com>
2019-01-08 12:10:31 -0600

    playing with functionality when button is pressed

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,6 +1,32 @@
 $(document).ready(function () {
 	$('button#loadButton').click(function(event) {
-        alert("help");
-	});	
+        $('#commentsSection').toggleClass("hidden unhidden");
+        debugger;
+        
+        var url = $(this).data("url")
+        $.get(url)
+          .done(function( data ) {
+              $('#comments').html("");
+    
+            $.each(data, function(index, value) {
+                var newComment = new Comment(value, url);
+                
+                var commentHtml = newComment.formatDisplay();
+                  $('#comments').append(commentHtml)
+                });
+    
+          });
+    });
 });
 
+function Comment(comment, url) {
+	this.id = comment.id
+    this.comment = comment.comment
+    this.dressId = comment.dress_id
+	this.commentUrl = url + '/' + comment.id
+};
+
+Comment.prototype.formatDisplay = function() {
+	var commentHtml = "<li><p><%= comment.comment %></p></li>";
+	return commentHtml;
+};
